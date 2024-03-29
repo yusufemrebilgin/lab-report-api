0056b6e542f877717ceaef26d20f8585e2043135
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-03-29 18:42:35 +0300

    Adding Initial Project Files

---
+++ src/test/java/com/example/labreportapi/LabreportapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labreportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabreportapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
