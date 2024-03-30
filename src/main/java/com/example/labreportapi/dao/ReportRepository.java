514f2518a35f32169bb9fda66cbdb777a6079075
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-03-31 00:17:17 +0300

    Update ReportRepository.java
    Adding a method to sort reports by date

--- src/main/java/com/example/labreportapi/dao/ReportRepository.java
+++ src/main/java/com/example/labreportapi/dao/ReportRepository.java
@@ -3,8 +3,11 @@ package com.example.labreportapi.dao;
 import com.example.labreportapi.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 
     boolean existsByReportCode(String reportCode);
+    List<Report> findAllByOrderByReportDetailReportDateAsc();
 
 }
