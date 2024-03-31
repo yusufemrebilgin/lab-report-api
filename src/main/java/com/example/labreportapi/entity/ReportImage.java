b93800feef124b122593ad754b3d132caedcc564
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-03-31 22:48:23 +0300

    Create ReportImage.java

---
+++ src/main/java/com/example/labreportapi/entity/ReportImage.java
@@ -0,0 +1,31 @@
+package com.example.labreportapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "report_image")
+public class ReportImage extends BaseEntity {
+    
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Column(name = "image_data", columnDefinition = "BLOB")
+    private byte[] imageData;
+
+    @JsonIgnore
+    @OneToOne(
+            mappedBy = "reportImage",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},
+            fetch = FetchType.LAZY)
+    private ReportDetail reportDetail;
+
+}
