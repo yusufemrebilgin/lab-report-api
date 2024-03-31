51a891379e4b21fbe1482b6a529e7c6508fc7eac
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-04-01 02:49:42 +0300

    Update PatientDetailRepository.java

--- src/main/java/com/example/labreportapi/dao/PatientDetailRepository.java
+++ src/main/java/com/example/labreportapi/dao/PatientDetailRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientDetailRepository extends JpaRepository<PatientDetail, Integer> {
 
+    boolean existsByIdentityNumber(long identityNumber);
+
 }
