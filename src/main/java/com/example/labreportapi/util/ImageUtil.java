5c0a43d1988709d2219c4e401e34d4b281084f1f
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-03-31 04:17:06 +0300

    Adding util class to process image data

---
+++ src/main/java/com/example/labreportapi/util/ImageUtil.java
@@ -0,0 +1,43 @@
+package com.example.labreportapi.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+public class ImageUtil {
+
+    private static final int BUFFER_SIZE = 1024;
+
+    public static byte[] compressImage(byte[] data) throws IOException {
+        Deflater deflater = new Deflater();
+        deflater.setLevel(Deflater.BEST_COMPRESSION);
+        deflater.setInput(data);
+        deflater.finish();
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while (!deflater.finished()) {
+                int count = deflater.deflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            return outputStream.toByteArray();
+        }
+    }
+
+    public static byte[] decompressImage(byte[] data) throws IOException, DataFormatException {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            return outputStream.toByteArray();
+        }
+    }
+
+}
