927e09253fa317dfb0dd3b8fff33e8a901401b97
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-03-31 22:51:02 +0300

    Update ReportDetailRepository.java

--- src/main/java/com/example/labreportapi/dao/ReportDetailRepository.java
+++ src/main/java/com/example/labreportapi/dao/ReportDetailRepository.java
@@ -3,6 +3,10 @@ package com.example.labreportapi.dao;
 import com.example.labreportapi.entity.ReportDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReportDetailRepository extends JpaRepository<ReportDetail, Integer> {
+
+    Optional<ReportDetail> findByReportId(int id);
 
 }
