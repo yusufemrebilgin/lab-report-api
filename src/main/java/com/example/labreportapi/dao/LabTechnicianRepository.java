1a3c4708d66584607d09ef3bc5c4efdfe4a2be0a
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-04-01 23:27:33 +0300

    Update repositories

--- src/main/java/com/example/labreportapi/dao/LabTechnicianRepository.java
+++ src/main/java/com/example/labreportapi/dao/LabTechnicianRepository.java
@@ -3,6 +3,11 @@ package com.example.labreportapi.dao;
 import com.example.labreportapi.entity.LabTechnician;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LabTechnicianRepository extends JpaRepository<LabTechnician, Integer> {
+
+    Optional<LabTechnician> findByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
+    LabTechnician findByHospitalId(int hospitalId);
 
 }
--- src/main/java/com/example/labreportapi/dao/PatientRepository.java
+++ src/main/java/com/example/labreportapi/dao/PatientRepository.java
@@ -3,6 +3,13 @@ package com.example.labreportapi.dao;
 import com.example.labreportapi.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+    Optional<Patient> findByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
+    Patient findByPatientDetailIdentityNumber(long identityNumber);
+    List<Patient> findAllByOrderByFirstNameAscLastNameAsc();
 
 }
