e14afdc91bf56955e1060afc9397e43119fe6b71
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-04-29 01:38:20 +0300

    Change class name

--- src/main/java/com/example/labreportapi/LabreportapiApplication.java
+++ src/main/java/com/example/labreportapi/LabreportapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LabreportapiApplication {
+public class LabReportApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LabreportapiApplication.class, args);
+		SpringApplication.run(LabReportApiApplication.class, args);
 	}
 
 }
